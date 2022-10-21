3fd24f71bbac514c552475e0e7b048545685890a
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-10-21 09:53:25 +0100

    Refactor code

--- src/ProductData.js
+++ src/ProductData.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-const today = new Date();
+const hours = new Date().getHours() > 9? new Date().getHours(): '0' + new Date().getHours(); 
+const minutes = new Date().getMinutes() > 9? new Date().getMinutes(): '0' + new Date().getMinutes(); 
+const time = hours + ':' + minutes;
+
+const heart = "💓";
 
 const ProductData = {
     title: 'FitBit 19 - The Smartest Watch',
@@ -31,12 +35,19 @@ const ProductData = {
 class FitBit19 extends React.Component {
 
     state = {
-        watchColor: 0
+        watchColor: 0,
+        watchFace: time
     }
 
     changeColor = (pos) => {
         this.setState(
             {watchColor:pos}
+        );
+    }
+
+    changeWatchFace = (face) => {
+        this.setState(
+            {watchFace:face}
         );
     }
 
@@ -50,6 +61,7 @@ class FitBit19 extends React.Component {
             <div className="Product-page">
                 <div className="Product-image">
                     <img src={ProductData.colorOptions[this.state.watchColor].imageUrl} className="main-product-image" alt="Fitbit Wristwatch"/>
+                    <div className="watchFace">{this.state.watchFace}</div>
                 </div>
                 <div className="Product-details">
                     <h1>{ProductData.title}</h1>
@@ -59,8 +71,8 @@ class FitBit19 extends React.Component {
                         {watchColors}
                     </div>
                     <h3>Features</h3>
-                    <button className="features-button time">Time</button>
-                    <button className="features-button heart-rate">Heart Rate</button>
+                    <button className="features-button time" onClick={() => this.changeWatchFace(time)}>Time</button>
+                    <button className="features-button heart-rate" onClick={() => this.changeWatchFace(heart)}>Heart Rate</button>
                     <button className="buy-now-button">Buy Now</button>
                 </div>
             </div>
